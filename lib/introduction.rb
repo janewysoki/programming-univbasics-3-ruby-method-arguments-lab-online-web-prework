41ff304fd5867959fb95fc8862db72a815a15faf
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-09 17:06:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,4 +11,4 @@ def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional()
+introduction_with_language_optional(name, "Ruby")
